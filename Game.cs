d236de5e27e0e17620e5969b535a1034b4675222
Justafigurehead <strachanmichaela@hotmail.com>
2017-05-24 14:39:14 +0100

    Initial Commit
    Initial file structure with map and point containing basic methods.

---
+++ Game.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TowerDefense
+{
+    class Game
+    {
+        public static void Main()
+        {
+            Map map = new Map(8, 5);
+
+            Point point = new Point(4, 2);
+            Console.WriteLine(point.DistanceTo(5, 5));
+            Console.ReadKey();
+            
+        }
+    }
+}
